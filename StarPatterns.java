141064535356151eb726306764dee0a81454f546
Kavindu Bimsara Fernando <164037G@UOM.lk>
2019-11-11 02:58:18 +0530

    Create StarPatterns.java

---
+++ StarPatterns.java
@@ -0,0 +1,78 @@
+//     * 
+//    * * 
+//   * * * 
+//  * * * * 
+// * * * * * 
+// 
+//public class StarPattern {
+//	public static void main(String[] args) {
+//		int n = 5;
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < (n - i); j++) {
+//				System.out.print(" ");
+//			}
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+//-------------------------------------------------------------------------------------------------------
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+//public class StarPattern {
+//	public static void main(String[] args) {
+//		int n = 5;
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < (n - i); j++) {
+//				System.out.print(" ");
+//			}
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+//-------------------------------------------------------------------------------------------------------
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//
+//public class StarPattern {
+//	public static void main(String[] args) {
+//		int n = 5;
+//		for (int i = 0; i < n; i++) {
+//			if (i % 2 == 1) {
+//				for (int j = 0; j < (n - i); j++) {
+//					System.out.print(" ");
+//				}
+//
+//			} else {
+//				for (int j = 0; j < (n - i); j++) {
+//					System.out.print(" ");
+//				}
+//			}
+//
+//			for (int j = 0; j <= 2*i; j++) {
+//				System.out.print("*");
+//			}
+//
+//			System.out.println();
+//		}
+//	}
+//}
+
